b7f5a09f2679429f246911283a1fa75c7b143238
SerebrovEL <serebrov_e_l@mail.ru>
2025-09-08 13:01:55 +0500

    Update BaseUrl.java

--- src/main/java/ru/neoflex/restclient/annotations/BaseUrl.java
+++ src/main/java/ru/neoflex/restclient/annotations/BaseUrl.java
@@ -5,9 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для указания базового URL для REST-сервиса.
+ * Применяется на уровне класса интерфейса сервиса.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface BaseUrl {
 
-    String value();
+  /**
+   * Базовый URL для всех запросов сервиса.
+   *
+   * @return строковое значение базового URL
+   */
+  String value();
 }
