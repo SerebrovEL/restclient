3719fcf640832212685395068f707e115ce83e3a
SerebrovEL <serebrov_e_l@mail.ru>
2025-09-08 13:04:23 +0500

    Update JsonField.java

--- src/main/java/ru/neoflex/restclient/annotations/JsonField.java
+++ src/main/java/ru/neoflex/restclient/annotations/JsonField.java
@@ -5,9 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для кастомного именования полей при сериализации в JSON.
+ * Применяется к полям классов.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface JsonField {
 
-    String value() default "";
+  /**
+   * Кастомное имя поля в JSON.
+   * Если не указано, используется имя поля класса.
+   *
+   * @return строковое значение имени поля в JSON
+   */
+  String value() default "";
 }
