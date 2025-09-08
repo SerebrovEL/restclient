71e83a30e33b3006921777b8761f49c92e266433
SerebrovEL <serebrov_e_l@mail.ru>
2025-09-08 13:02:18 +0500

    Update Body.java

--- src/main/java/ru/neoflex/restclient/annotations/Body.java
+++ src/main/java/ru/neoflex/restclient/annotations/Body.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для обозначения параметра метода как тела запроса.
+ * Применяется к параметрам методов REST-сервиса.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface Body {
