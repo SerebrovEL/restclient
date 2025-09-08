ba626705c44e7717563c270f630fc7daaa8802ca
SerebrovEL <serebrov_e_l@mail.ru>
2025-09-08 13:09:04 +0500

    Update ResponseInterceptor.java

--- src/main/java/ru/neoflex/restclient/interceptors/ResponseInterceptor.java
+++ src/main/java/ru/neoflex/restclient/interceptors/ResponseInterceptor.java
@@ -1,6 +1,17 @@
 package ru.neoflex.restclient.interceptors;
 
+/**
+ * Интерфейс для перехватчиков HTTP ответов.
+ * Позволяет обрабатывать ответ перед возвратом клиенту.
+ */
 public interface ResponseInterceptor {
 
-    String intercept(int responseCode, String response);
+  /**
+   * Перехватывает и потенциально модифицирует HTTP ответ.
+   *
+   * @param responseCode HTTP код ответа
+   * @param response тело ответа
+   * @return обработанное тело ответа
+   */
+  String intercept(int responseCode, String response);
 }
