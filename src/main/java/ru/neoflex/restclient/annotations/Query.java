2b313cb4262b565ede26dcfaa4914bbcc7796f99
SerebrovEL <serebrov_e_l@mail.ru>
2025-09-08 13:06:44 +0500

    Update Query.java

--- src/main/java/ru/neoflex/restclient/annotations/Query.java
+++ src/main/java/ru/neoflex/restclient/annotations/Query.java
@@ -5,9 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для указания параметра запроса (query parameter) в URL.
+ * Применяется к параметрам методов REST-сервиса.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface Query {
 
-    String value();
+  /**
+   * Имя параметра запроса.
+   *
+   * @return строковое значение имени параметра запроса
+   */
+  String value();
 }
