66f1b8444c9794e1578ec4183c007c409df7f8f1
SerebrovEL <serebrov_e_l@mail.ru>
2025-09-08 13:04:46 +0500

    Update JsonObject.java

--- src/main/java/ru/neoflex/restclient/annotations/JsonObject.java
+++ src/main/java/ru/neoflex/restclient/annotations/JsonObject.java
@@ -5,9 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для указания кастомного имени корневого объекта при сериализации в JSON.
+ * Применяется к классам.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface JsonObject {
 
-    String value() default "";
+  /**
+   * Кастомное имя корневого объекта в JSON.
+   * Если не указано, используется имя класса.
+   *
+   * @return строковое значения имени корневого объекта
+   */
+  String value() default "";
 }
